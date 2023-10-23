c18ace2d5f7ab5873cd9815c36d3fd5ab26b15f0
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-10-23 15:58:50 +0500

    Task7

---
+++ Tyuiu.GrigorjanAM.Sprint3.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorjanAM.Sprint3.Task7.V14.Lib;
+
+namespace Tyuiu.GrigorjanAM.Sprint3.Task7.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startvalue = -2;
+            int stopvalue = 2;
+            int len = stopvalue - startvalue + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -63.13;
+            valueWaitArray[1] = -24.92;
+            valueWaitArray[2] = -5.0;
+            valueWaitArray[3] = -1.46;
+            valueWaitArray[4] = 1.57;
+            double[] res;
+            res = ds.GetMassFunction(startvalue, stopvalue);
+            CollectionAssert.AreEqual(res, valueWaitArray);
+
+        }
+    }
+}
---
+++ Tyuiu.GrigorjanAM.Sprint3.Task7.V14/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GrigorjanAM.Sprint3.Task7.V14.Lib;
+
+namespace Tyuiu.GrigorjanAM.Sprint3.Task7.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Григорян А. М. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Григорян Арсен Мовсесович | ИИПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, выводящую таблицу значений функции,                 *");
+            Console.WriteLine("* на диапазоне [-5,5] с шагом 1. Проверить деление на ноль => вернуть ноль*");
+            Console.WriteLine("* где функция - F(x) = 2x - 4  + (2x-1) / (sin(x) + 1)                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startvalue = -5;
+            int stopvalue = 5;
+            Console.WriteLine("Старт шага = " + startvalue);
+            Console.WriteLine("Конец шага = " + stopvalue);
+
+            int len = ds.GetMassFunction(startvalue, stopvalue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startvalue, stopvalue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+-----------+-----------+");
+            Console.WriteLine("|     X     |    f(x)   |");
+            Console.WriteLine("+-----------+-----------+");
+            for(int i = 0; i <=len-1; i++)
+            {
+                Console.WriteLine("|{0,6:d}     |  {1,6:f2}   |", startvalue, valueArray[i]);
+                startvalue++;
+            }
+            Console.WriteLine("+-----------+-----------+");
+            Console.ReadKey();
+
+        }
+    }
+}
